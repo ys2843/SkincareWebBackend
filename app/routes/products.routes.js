ee5ca4bf194cd24063903a1e010052db1cadd1d1
ys2843 <ys2843@nyu.edu>
2018-03-27 11:40:08 -0400

    fix cors

--- app/routes/products.routes.js
+++ app/routes/products.routes.js
@@ -1,6 +1,10 @@
 module.exports = function (app) {
     const products = require('../controllers/products.controller')
-
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
     app.get('/api/search/:product', products.findAll);
 
 }
