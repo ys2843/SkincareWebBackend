a56908b25df3d559e4d374ad1aeeb705909b0f68
ys2843 <ys2843@nyu.edu>
2018-03-28 23:12:15 -0400

    content-length

--- app/routes/products.routes.js
+++ app/routes/products.routes.js
@@ -3,6 +3,7 @@ module.exports = function (app) {
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Expose-Headers', 'Content-Length');
         next();
     });
     app.get('/api/search/:product/:pageNumber/:lazyLoadCount', products.findAll);
