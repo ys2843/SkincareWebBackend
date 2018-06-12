29bed199440ce79809a777a80b6cb3a45e662736
ys2843 <ys2843@nyu.edu>
2018-06-11 22:35:26 -0400

    fix pagination

--- app/routes/products.routes.js
+++ app/routes/products.routes.js
@@ -1,10 +1,11 @@
 module.exports = function (app) {
     const products = require('../controllers/products.controller')
     app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header('Access-Control-Expose-Headers', 'Content-Length');
-        res.header('Content-Type', 'application/json');
+        res.append("Access-Control-Allow-Origin", "*");
+        res.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.append('Access-Control-Expose-Headers', 'Content-Length');
+        res.append('Content-Type', 'application/json');
         next();
     });
     app.get('/api/search/:product/:pageNumber/:nPerPage', products.findAll);
