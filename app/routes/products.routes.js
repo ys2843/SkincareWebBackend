4670251d4be956e393ae1e747fc836e84ae29324
ys2843 <ys2843@nyu.edu>
2018-06-11 22:51:31 -0400

    update

--- app/routes/products.routes.js
+++ app/routes/products.routes.js
@@ -1,13 +1,7 @@
 module.exports = function (app) {
+    const cors = require('cors');
     const products = require('../controllers/products.controller')
-    app.use(function (req, res, next) {
-        res.append("Access-Control-Allow-Origin", "*");
-        res.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.append('Access-Control-Expose-Headers', 'Content-Length');
-        res.append('Content-Type', 'application/json');
-        next();
-    });
+    app.use(cors());
     app.get('/api/search/:product/:pageNumber/:nPerPage', products.findAll);
     app.get('/api/:product/count', products.findCount);
 
