a1a4f996cbe8da04c747ac2483c98dde04f9d69c
ys2843 <ys2843@nyu.edu>
2018-06-11 23:26:58 -0400

    update

--- app/routes/products.routes.js
+++ app/routes/products.routes.js
@@ -2,6 +2,9 @@ module.exports = function (app) {
     const cors = require('cors');
     const products = require('../controllers/products.controller')
     app.use(cors());
+    app.get('/', function (req,res) {
+        res.send('Hello world');
+    })
     app.get('/api/search/:product/:pageNumber/:nPerPage', products.findAll);
     app.get('/api/:product/count', products.findCount);
 
